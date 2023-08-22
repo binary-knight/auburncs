ea596e537796e6f485f4609d911bb899766a6530
cj6046 <87824435+cj6046@users.noreply.github.com>
2023-08-22 07:43:03 -0400

    new update.sh script - frontend implementation only

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# variables
+BACKEND_DIR = "/backend"
+FRONTEND_DIR = "/frontend"
+BACKEND_DEST = "/var/www/backend"
+FRONTEND_DEST = "/var/www/html/auburncs-dev"
+
+update_backend_dev() {
+
+}
+
+update_backend_prod() {
+
+}
+
+update_frontend_dev() {
+    cd $FRONTEND_DIR
+    npm install
+    rm -rf $FRONTEND_DEST/* # clears previous frontend files
+    npm run build:dev
+    mv build/* $FRONTEND_DEST
+}
+
+update_frontend_prod() {
+    cd $FRONTEND_DIR
+    npm install
+    rm -rf $FRONTEND_DEST # clears previous frontend files
+    npm run build:prod
+    mv build/* $FRONTEND_DEST
+}
+
+start() {
+    if [ "$1" = "prod" ]; then
+        echo "Building for production..."
+        update_frontend_prod
+    elif [ "$1" = "dev" ]; then
+        echo "Building for development..."
+        update_frontend_dev
+    else
+        echo "Invalid argument. Please use 'prod' or 'dev'."
+    fi
+    echo "Build complete!"
+}
+
+start
+
